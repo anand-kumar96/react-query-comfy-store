eda30d2858014ed67398407387cf74e191c66329
Anand Kumar <hyeiamaka06@gmail.com>
2024-05-21 19:37:52 +0530

    newsletter added

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -19,7 +19,7 @@ const NewsLetter = () => {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
           <div className="max-w-xl lg:max-w-lg">
             <h2 className="text-3xl font-bold tracking-tight text-primary sm:text-4xl">Subscribe to our newsletter.</h2>
-            <p className="mt-4 text-lg leading-8 text-gray-300">
+            <p className="mt-4 text-lg leading-8">
             Discover stylish, handcrafted furniture and exclusive offers. Transform your home with our tips and special deals.
             </p>
             <div className="mt-6 flex max-w-md gap-x-4">
@@ -32,13 +32,13 @@ const NewsLetter = () => {
                 autoComplete="email"
                 value={email}
                 required
-                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                className="min-w-0 flex-auto rounded-md border-0 bg-base-200 px-3.5 py-2  shadow-sm ring-1 ring-inset  focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
                 placeholder="Enter your email"
                 onChange={handleChange}
                 />
               <button
                 type="submit"
-                className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="rounded-md btn btn-primary font-semibold"
                 onClick={handleSubmit}
                 >
                 Subscribe
@@ -48,19 +48,19 @@ const NewsLetter = () => {
           <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
             <div className="flex flex-col items-start">
               <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                <CalendarDaysIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                <CalendarDaysIcon className="h-6 w-6 " aria-hidden="true" />
               </div>
-              <dt className="mt-4 font-semibold text-white">Weekly articles</dt>
-              <dd className="mt-2 leading-7 text-gray-400">
+              <dt className="mt-4 font-semibold">Weekly articles</dt>
+              <dd className="mt-2 leading-7">
               Discover fresh insights and inspiration in our weekly articles covering trends, tips, and more.
               </dd>
             </div>
             <div className="flex flex-col items-start">
               <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                <HandRaisedIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                <HandRaisedIcon className="h-6 w-6 " aria-hidden="true" />
               </div>
-              <dt className="mt-4 font-semibold text-white">No spam</dt>
-              <dd className="mt-2 leading-7 text-gray-400">
+              <dt className="mt-4 font-semibold ">No spam</dt>
+              <dd className="mt-2 leading-7">
               Engage with our diverse range of weekly articles for valuable insights, inspiration, and expert advice.
               </dd>
             </div>
